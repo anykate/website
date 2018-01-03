f54aa4dad043f3451b34ecebdba28a90f5c07b38
Aniket Aryamane <aryamane.aniket@gmail.com>
2018-01-03 17:23:17 +0530

    Final Commit
    Final Commit

--- guestbook/models.py
+++ guestbook/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+	name = models.CharField(max_length=20)
+	comment = models.TextField()
+	date_created = models.DateTimeField(auto_now_add=True)
+	date_updated = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return 'Name: {}; ID:{}'.format(self.name, self.id)
--- guestbook/views.py
+++ guestbook/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from .models import Comment
+from .forms import CommentForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
-	return render(request, 'guestbook/index.html', {})
+	comments = Comment.objects.order_by('-date_created')
+	return render(request, 'guestbook/index.html', {'comments':comments})
 
 def addcomments(request):
-	return render(request, 'guestbook/addcomments.html', {})
+	if request.method == 'POST':
+		form = CommentForm(request.POST)
+
+		if form.is_valid():
+			new_comment = Comment(name=request.POST['name'], comment = request.POST['comment'])
+			new_comment.save()
+			return redirect('guestbook:index')
+
+	else:
+		form = CommentForm()
+
+	return render(request, 'guestbook/addcomments.html', {'form':form})
